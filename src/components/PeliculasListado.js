a51ea872299baa230d49bacbae43fd4b3746cf68
Agustín Durán <agustineduran@gmail.com>
2022-07-21 15:50:07 -0300

    refact

--- src/components/PeliculasListado.js
+++ src/components/PeliculasListado.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
 const PeliculasListado = (props) => {
-    const { favoritoHandler } = props;
-    const FavoritoComponente = props.favoritoComponente;
-    const SinResultadosComponente = props.sinResultadosComponente;
+  const { favoritoHandler } = props;
+  const FavoritoComponente = props.favoritoComponente;
+  const SinResultadosComponente = props.sinResultadosComponente;
 
-    return (
-        <div className={ "row " + (props.peliculas.length > 0 ? 'rows-cols-1 row-cols-md-3 g-3': '') }>
-            {
-                props.peliculas.length > 0 
-                ? props.peliculas.map((pelicula, index) => (
-                        <div className='col' key={`cart-${index}`} >
-                            <div className='image-container card mt-4'>
-                                <img src={pelicula.Poster} alt={pelicula.Title} ></img>
-                                <div 
-                                    onClick={() => favoritoHandler(pelicula)}
-                                    className='overlay d-flex align-items-center justify-content-center'
-                                >
-                                    <FavoritoComponente/>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                : <SinResultadosComponente/>
-            }
-        </div>
-    );
+  return (
+    <div className={"row " + (props.peliculas.length > 0 ? 'rows-cols-1 row-cols-md-3 g-3' : '')}>
+      {
+        props.peliculas.length > 0
+          ? props.peliculas.map((pelicula, index) => (
+            <div className='col' key={`cart-${index}`} >
+              <div className='image-container card mt-4'>
+                <img src={pelicula.Poster} alt={pelicula.Title} ></img>
+                <div
+                  onClick={() => favoritoHandler(pelicula)}
+                  className='overlay d-flex align-items-center justify-content-center'
+                >
+                  <FavoritoComponente />
+                </div>
+              </div>
+            </div>
+          ))
+          : <SinResultadosComponente />
+      }
+    </div>
+  );
 }
 
 export default PeliculasListado;
